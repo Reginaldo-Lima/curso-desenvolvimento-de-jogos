237a5ed91f2a3cb05702a5debbc31c7003d54443
Reginaldo-Lima <reginaldomoura33@gmail.com>
2023-10-31 16:04:05 -0300

    import-export-js

---
+++ javascript/07 importação/main.js
@@ -0,0 +1,27 @@
+
+const {gets, print} = require('./funcoes-auxiliares');
+
+print(gets());
+
+/*
+
+const funcoes = require('./funcoes-auxiliares');
+
+
+console.log(funcoes);
+*/
+
+/*
+// OBJECT DESTRUCTURING
+
+const pessoa = {
+    nome: 'Vitor'
+}
+
+const { nome } = pessoa;
+
+// o código a cima é o mesmo que o de baixo.
+
+const nome = pessoa.nome;
+
+*/
